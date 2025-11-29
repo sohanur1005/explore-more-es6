19cc488dce9c70c9acc7bd453b3ae49d527d12a1
sohanur1005 <sohanur.sr1005@gmail.com>
2025-11-29 23:28:05 +0600

    send this

--- chain.js
+++ chain.js
@@ -2,3 +2,15 @@
 const data=[{id: 1,name: 'sohanur',address:'Rajshahi'}]
 console.log(data)
 console.log(data[0].address)
+
+const products={
+    count:500,
+    data:[
+        {id:1, name:'Lenovo Laptop', price:50000},
+        {id:2, name:'Macbook', price:150000}
+    ]
+}
+
+
+// second product price
+console.log(products.data[1].price)
