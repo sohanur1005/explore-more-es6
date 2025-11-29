d3b66bdae85abed995cdb2d879276a29ac0fe7bc
sohanur1005 <sohanur.sr1005@gmail.com>
2025-11-29 23:48:27 +0600

    send this

--- chain.js
+++ chain.js
@@ -33,3 +33,17 @@ const user={
 // access  user address second street
 
 console.log(user.address.street.second)
+
+
+// optional chaining
+
+const user2={
+    id:1010,
+    name:'Doyal Baba',
+    address: {
+        city:'Dhaka',
+        country:'Bangladesh'
+    }
+}
+// ? the sign use when if the street value is available the go otherwise stop..As a rsult  no error be found
+console.log(user2.address.street?.second)
