1690ebde18bd569106ffd66125afd3b7c71899ad
sohanur1005 <sohanur.sr1005@gmail.com>
2025-12-01 13:14:52 +0600

    send this

--- more-map.js
+++ more-map.js
@@ -1,3 +1,17 @@
 const numbers=[5,7,8,9,3];
 const doubled=numbers.map(num=>num *2)
 console.log(doubled)
+
+const fiveBonus=numbers.map(num=>(num*2)+5)
+console.log(fiveBonus)
+
+
+const friends=['sohnaur','rahman','sohag','mahfuz'];
+
+const length=friends.map(frnd=>frnd.length)
+console.log(length)
+// show first letter
+const firsrtLetter=friends.map(frnd=>frnd[0])
+console.log(firsrtLetter)
+
+
