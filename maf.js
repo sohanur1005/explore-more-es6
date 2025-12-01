bc300c79a6e5f76bcececa405ca0d11d244b6dd0
sohanur1005 <sohanur.sr1005@gmail.com>
2025-12-01 12:05:27 +0600

    send this

--- maf.js
+++ maf.js
@@ -1,16 +1,17 @@
 const number=[4,5,7,8,6];
-const doubled=[];
-for( const num of number){
-    const double=num*2;
-    doubled.push(double);
+// const doubled=[];
+// for( const num of number){
+//     const double=num*2;
+//     doubled.push(double);
+// }
+// console.log(doubled)
+
+// map loop through each element of the array  and do the operation that you passed in the call back function and hold the result form each operation in an array and finally returns the array
+
+function double2(num){
+    console.log('num now', num)
+    return num*2;
 }
-console.log(doubled)
 
-// using map
-
-const doubleIT(number){
-    const result =number *2;
-    return result;
-}
-
-const result =number.map(doubleIT)
+const result =number.map(double2)
+console.log(result)
