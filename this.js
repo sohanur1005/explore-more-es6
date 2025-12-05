cae148b7773387b2738d783e0c054c141deec56f
sohanur1005 <sohanur.sr1005@gmail.com>
2025-12-05 23:43:34 +0600

    send this portion

--- this.js
+++ this.js
@@ -1,15 +1,16 @@
 class Person{
-    constructor(name,age){
+    constructor(name,age,location){
         this.name=name;
         this.age=age;
+        this.location=location;
     }
     sleep(){
         console.log(`Sleeping now ${this.name}`);
     }
 }
 
-const sohanur=new Person('Sohanur Rahman',23)
-const rahman=new Person('Rahman Bintea',25)
+const sohanur=new Person('Sohanur Rahman',23,'Dhaka')
+const rahman=new Person('Rahman Bintea',25,'Rajshahi')
 console.log(sohanur)
 console.log(rahman)
 sohanur.sleep();
